551c33e3dbbc27f50d2e68a30c50d29126200a40
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-06-24 00:27:42 +0200

    url backend

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apis = {
   development: 'http://localhost:4000',
-  production: 'https://organize-me-app.netlify.app',
+  production: 'https://ih-organize-me.herokuapp.com/',
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
